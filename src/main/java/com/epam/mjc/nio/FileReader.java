6ede9ec20150ff617bb23805b36f1d12dc98cea9
Baimenovbn <baibn@mail.ru>
2022-12-28 17:12:02 +0600

    feat: finish task

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,50 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileReader {
+    static private final Logger LOGGER = Logger.getLogger(FileReader.class.getName());
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Path path = file.toPath();
+        Profile profile = new Profile();
+
+        try (BufferedReader in = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] lineArr = line.split(" ");
+                switch (getFieldName(lineArr)) {
+                    case "Age":
+                        profile.setAge(Integer.parseInt(lineArr[1]));
+                        break;
+                    case "Name":
+                        profile.setName(lineArr[1]);
+                        break;
+                    case "Email":
+                        profile.setEmail(lineArr[1]);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(lineArr[1]));
+                        break;
+                    default:
+                        LOGGER.log(Level.SEVERE, () -> "Wrong field name" + getFieldName(lineArr));
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
+    }
+
+    static private String getFieldName(String[] line) {
+        return line[0].substring(0, line[0].length() - 1);
     }
 }
